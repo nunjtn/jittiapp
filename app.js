1c651173c69d99fbcc2d7b3cb96e274a2d0274d5
nunjtn <nun20270@gamil.com>
2021-07-23 00:55:54 +0700

    remove space

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ router.get('/',function(req,res){
     res.sendFile(path + 'index.html');
 });
 
-router.get('/sharks', function(req,res){
+router.get('/sharks',function(req,res){
     res,sendFile(path + 'sharks.html')
 });
 
